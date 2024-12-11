6335afe62b70aeeb792d89957b8188be835a3521
Sunil Nath <Sunil.nath@students.dominican.edu>
2024-12-11 15:08:16 -0800

    added abilities and armor

--- ability.py
+++ ability.py
@@ -1,7 +1,18 @@
+import random
+
 class Ability:
     def __init__(self, name, max_damage):
+        '''Initialize the values passed into this method as instance variables.'''
         self.name = name
-        self.max_damage = int(max_damage)
+        self.max_damage = max_damage
 
-    def use(self):
-        return self.max_damage
+    def attack(self):
+        '''Return a value between 0 and the value set by self.max_damage.'''
+        # Pick a random value between 0 and self.max_damage
+        return random.randint(0, self.max_damage)
+
+if __name__ == "__main__":
+    # Test the Ability class
+    ability = Ability("Debugging Ability", 20)
+    print(ability.name)
+    print(ability.attack())
--- armor.py
+++ armor.py
@@ -1,7 +1,18 @@
-class Armor:
-    def __init__(self, name, defense):
-        self.name = name
-        self.defense = int(defense)
+import random
 
-    def defend(self):
-        return self.defense
+class Ability:
+    def __init__(self, name, max_damage):
+        '''Initialize the values passed into this method as instance variables.'''
+        self.name = name
+        self.max_damage = max_damage
+
+    def attack(self):
+        '''Return a value between 0 and the value set by self.max_damage.'''
+        # Pick a random value between 0 and self.max_damage
+        return random.randint(0, self.max_damage)
+
+if __name__ == "__main__":
+    # Test the Ability class
+    ability = Ability("Debugging Ability", 20)
+    print(ability.name)
+    print(ability.attack())
