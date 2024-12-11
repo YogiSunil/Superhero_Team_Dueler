609a8155354f3b58b349248ea04deeaa29f3ac5e
Sunil Nath <Sunil.nath@students.dominican.edu>
2024-12-11 15:14:17 -0800

    Implemented team attack and defense

--- hero.py
+++ hero.py
@@ -2,32 +2,31 @@ from ability import Ability
 from weapon import Weapon
 
 class Hero:
-    def __init__(self, name):
+    def __init__(self, name, health=100):
         self.name = name
-        self.abilities = []
+        self.health = health
+        self.starting_health = health  # To keep track of original health
+        self.deaths = 0
+        self.kills = 0
+        # Add any other attributes you already have
+        # ...
 
-    def add_ability(self, ability):
-        """Add an Ability to the hero's abilities list."""
-        self.abilities.append(ability)
+    def add_kill(self, num_kills):
+        ''' Update self.kills by num_kills amount '''
+        self.kills += num_kills
 
-    def add_weapon(self, weapon):
-        """Add a Weapon to the hero's abilities list."""
-        self.abilities.append(weapon)
+    def add_death(self, num_deaths):
+        ''' Update deaths with num_deaths '''
+        self.deaths += num_deaths
 
-    def attack(self):
-        """Calculate the total damage from all abilities."""
-        total_damage = 0
-        for ability in self.abilities:
-            total_damage += ability.attack()
-        return total_damage
-
-if __name__ == "__main__":
-    # Test Hero functionality
-    hero = Hero("Wonder Woman")
-    weapon = Weapon("Lasso of Truth", 90)
-    ability = Ability("Super Strength", 50)
-
-    hero.add_ability(ability)
-    hero.add_weapon(weapon)
-
-    print(f"{hero.name} attacks with total damage: {hero.attack()}")
+    def fight(self, opponent):
+        '''Simulate fight and update stats'''
+        if self.health > opponent.health:
+            opponent.add_death(1)  # Opponent dies
+            self.add_kill(1)  # Hero gets a kill
+        elif self.health < opponent.health:
+            self.add_death(1)  # Hero dies
+            opponent.add_kill(1)  # Opponent gets a kill
+        else:
+            # If health is equal, nobody dies
+            pass
--- team.py
+++ team.py
@@ -1,3 +1,5 @@
+import random
+
 class Team:
     def __init__(self, name):
         ''' Initialize your team with its team name and an empty list of heroes '''
@@ -5,11 +7,11 @@ class Team:
         self.heroes = list()
 
     def add_hero(self, hero):
-        '''Add Hero object to self.heroes.'''
+        ''' Add Hero object to self.heroes '''
         self.heroes.append(hero)
 
     def remove_hero(self, name):
-        '''Remove hero from heroes list. If Hero isn't found return 0.'''
+        ''' Remove hero from heroes list. If Hero isn't found, return 0. '''
         foundHero = False
         for hero in self.heroes:
             if hero.name == name:
@@ -19,6 +21,35 @@ class Team:
             return 0
 
     def view_all_heroes(self):
-        '''Prints out all heroes to the console.'''
+        ''' Print all heroes to the console '''
         for hero in self.heroes:
             print(hero.name)
+
+    def revive_heroes(self, health=100):
+        ''' Reset all heroes' health to starting_health '''
+        for hero in self.heroes:
+            hero.health = hero.starting_health
+
+    def stats(self):
+        ''' Print team statistics (Kill/Death ratio) '''
+        for hero in self.heroes:
+            if hero.deaths > 0:
+                kd_ratio = hero.kills / hero.deaths
+            else:
+                kd_ratio = hero.kills  # If no deaths, just show kills
+            print(f"{hero.name} Kill/Deaths: {kd_ratio}")
+
+    def attack(self, other_team):
+        ''' Battle each team against each other. '''
+        living_heroes = [hero for hero in self.heroes if hero.health > 0]
+        living_opponents = [hero for hero in other_team.heroes if hero.health > 0]
+
+        while len(living_heroes) > 0 and len(living_opponents) > 0:
+            hero = random.choice(living_heroes)  # Randomly select a hero
+            opponent = random.choice(living_opponents)  # Randomly select an opponent
+
+            hero.fight(opponent)  # They fight
+            if hero.health <= 0:
+                living_heroes.remove(hero)  # Hero died
+            if opponent.health <= 0:
+                living_opponents.remove(opponent)  # Opponent died
